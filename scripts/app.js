49cb12c59a0f475f05f1704d244cc6701e5c3752
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2023-11-28 17:59:53 +0100

    Code optimalization
    Trying to use general classes for elements

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,13 @@
 const button = document.querySelectorAll(".button");
 const rules = document.querySelector(".rules");
 const rulesContainer = document.querySelector(".rules-container");
+const main = document.querySelector("main");
+const playerChoiceContainer = document.querySelector(
+  ".player-choice-container"
+);
+const computerChoiceContainer = document.querySelector(
+  ".computer-choice-container"
+);
 
 const possibleScenarios = [
   {
@@ -17,13 +24,29 @@ const possibleScenarios = [
   },
 ];
 
+const updatePhase1 = () => {
+  const section = document.createElement("section");
+  section.classList.add("phase-1");
+  main.prepend(section);
+};
+
+const updatePhase2 = () => {
+  return phase2;
+};
+const updatePhase3 = () => {
+  return phase3;
+};
+const updatePhase4 = () => {
+  return phase4;
+};
+
+setTimeout(() => {}, 1000);
+
 button.forEach((active) => {
   active.addEventListener("click", (event) => {
     const computerChoice = getComputerId();
     const playerChoice = getPlayerId(event);
     const result = compareResult(computerChoice, playerChoice);
-    console.log(playerChoice);
-    console.log(computerChoice);
     saveResultToLocalStorage(result);
   });
 });
@@ -39,3 +62,5 @@ const updateRulesView = () => {
 rules.addEventListener("click", () => {
   updateRulesView();
 });
+
+updatePhase1();
